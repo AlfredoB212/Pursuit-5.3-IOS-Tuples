f234fcac4292cda49470e8294622c055a44dbcf7
AlfredoB212 <alfredobarragan@Pursuits-MacBook-Pro-93.local>
2018-10-05 12:56:50 -0400

    added tuples exercise

--- Tuples/main.swift
+++ Tuples/main.swift
@@ -8,5 +8,53 @@
 
 import Foundation
 
-print("Hello, World!")
+//Tuples
 
+//let fellow = (firstName:"Alex", lastName: "Paul", age: 39, tall: true)
+//
+//
+//print(fellow.age)
+//if fellow.tall {
+//    print("that's a tall human")
+//} else {
+//    print("maybe fit for a professor")
+//}
+//
+//let coordinate = (1, 1)
+//
+//switch fellow {
+//case ("Alex", "Paul", 39, true):
+//    print("welcome Alex")
+//case (_, _, _, _):
+//    print("welcome human")
+//default:
+//    print("what's going on my guy")
+//}
+
+
+let Kathy = (firstName: "Kathy", lastName: "M", age:19, smoker: false)
+let Joshua = (firstName: "Joshua", lastName: "V", age:40, smoker: true)
+
+if Joshua.smoker {
+    print("not interested")
+    
+    
+    
+    
+    
+    
+/*
+ if(condition) {only enteres if block if condition is true
+} else { // if condition is false
+ 
+ }
+ 
+ 
+ switch value{
+ case 0: //
+ print("executes")
+ case 1:
+ case 2:
+ default:
+    print("here's my number")
+    }
